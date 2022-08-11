70076835641c2e872f865f66e19d1bd86cb4b4fb
Randy Baukovac <rabbaukovac@yahoo.com>
2022-08-11 08:18:56 -0700

    Initial commit

---
+++ cookies.py
@@ -0,0 +1,10 @@
+def cookiesBag():
+    print('How many cookies did you eat?')
+    cookieMonster = input()
+    cookieCal = int(75)
+
+    print('You had', cookieCal * int(cookieMonster),  ' calories')
+
+
+
+cookiesBag()
